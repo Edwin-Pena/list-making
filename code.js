8d171a38c016077dff041175795ecd0eff5807b6
Edwin Peña <edwinsp095@gmail.com>
2024-08-05 21:36:32 -0500

    username validation to continue

--- code.js
+++ code.js
@@ -1,1 +1,30 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    const nameWindow = document.querySelector(".name-window");
+    const projectsWindow = document.querySelector(".projects");
+    const continueBtn = document.querySelector(".name-window__boton");
+
+    /* const username = usernameInput.value; */
+    const usernameSaved = localStorage.getItem("username");
+
+    if (usernameSaved) {
+        nameWindow.style.display = "none";
+        projectsWindow.style.display = "block";
+    } else {
+        continueBtn.addEventListener("click", (e)=> {
+            e.preventDefault();
+
+            const usernameInput = document.getElementById("username");
+            const username = usernameInput.value;
+        
+            localStorage.setItem("username", username);
+        
+            nameWindow.style.display = "none";
+            projectsWindow.style.display = "block";
+        
+            console.log("El nombre del usuario es " + username);
+        })
+    }
+})
