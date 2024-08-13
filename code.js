87dc7b561e82145319a06817a1b918db630d2d54
Edwin Peña <edwinsp095@gmail.com>
2024-08-13 06:37:03 -0500

    modification of classes with BEM metodology

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const inputSearch = document.querySelector(".projects__search");
-const closeBtn = document.querySelector(".projects__close");
+const inputSearch = document.querySelector(".nav__search");
+const closeBtn = document.querySelector(".nav__search-close");
 
 document.addEventListener("DOMContentLoaded", ()=> {
 
     const nameWindow = document.querySelector(".name-window");
     const projectsWindow = document.querySelector(".projects");
-    const continueBtn = document.querySelector(".name-window__boton");
+    const continueBtn = document.querySelector(".continue");
     const usernameSaved = localStorage.getItem("username");
 
     if (usernameSaved) {
