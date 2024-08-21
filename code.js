ff4c017b7720b42cce6af897c41159c672adfb11
Edwin Peña <edwinsp095@gmail.com>
2024-08-21 15:30:18 -0500

    Improving code for maintainability  - All the styles that were being modified directly with the javascript code were removed and now only with javascript the classes are added and removed depending on the need or specific case.

--- code.js
+++ code.js
@@ -1,10 +1,26 @@
 "use strict";
 
+/* Global constants */
 const inputSearch = document.querySelector(".nav__search");
-const closeBtn = document.querySelector(".nav__search-close");
-
+const closeSearchBtn = document.querySelector(".nav__search-close");
 const usernameInput = document.getElementById("username");
 
+/* Hidden and visible state of an element */
+const hiddenState = (e)=> {
+    if (e.classList.contains("visible")) {
+        e.classList.remove("visible");
+        e.classList.add("hidden");
+    }
+}
+
+const visibleState = (e)=> {
+    if (e.classList.contains("hidden")) {
+        e.classList.remove("hidden");
+        e.classList.add("visible");
+    }
+}
+
+/* Function that detects whether the user already has the name saved to display or not the page that contains the projects */
 document.addEventListener("DOMContentLoaded", ()=> {
 
     const nameWindow = document.querySelector(".name-window");
@@ -13,11 +29,12 @@ document.addEventListener("DOMContentLoaded", ()=> {
     const usernameSaved = localStorage.getItem("username");
 
     if (usernameSaved) {
-        nameWindow.style.display = "none";
-        projectsWindow.style.display = "block";
+        nameWindow.classList.remove("visible-flex");
+        visibleState(projectsWindow);
     } else {
-        nameWindow.style.display = "flex";
+        nameWindow.classList.add("visible-flex");
 
+        /* Function to detect if the name entered by the user is valid as it has 4 or more letters */
         const functionName = ()=> {
             const labelLine = document.querySelector(".input__label-line");
             const username = usernameInput.value.trim();
@@ -27,24 +44,25 @@ document.addEventListener("DOMContentLoaded", ()=> {
             if (nameValidation < 4) {
                 usernameInput.classList.add("error");
                 labelLine.classList.add("error");
-                invalidName.style.display = "block";
+                visibleState(invalidName);
                 return;
             }
         
             localStorage.setItem("username", username);
         
-            nameWindow.style.display = "none";
-            projectsWindow.style.display = "block";
+            nameWindow.classList.remove("visible-flex");
+            visibleState(projectsWindow);
         
             console.log("El nombre del usuario es " + username);
         }
 
-
+        /* Event that detects that we press the continue button and activates the function that validates the name to be able to continue */
         continueBtn.addEventListener("click", (e)=> {
             e.preventDefault();
             functionName();
         })
 
+        /* Event that detects that we press the enter key to continue and activates the function that validates the name just like the function of the previous button */
         usernameInput.addEventListener("keydown", (e)=> {
             if (e.key === "Enter") {
                 e.preventDefault();
@@ -54,22 +72,23 @@ document.addEventListener("DOMContentLoaded", ()=> {
     }
 })
 
-
+/* Event that displays the button that removes the text from the search input */
 inputSearch.addEventListener("input", (e)=> {
     e.preventDefault();
-    const value = inputSearch.value.trim()
+    const value = inputSearch.value;
 
     if (value) {
-        closeBtn.style.display = "block";
+        visibleState(closeSearchBtn);
     } else {
-        closeBtn.style.display = "none";
+        hiddenState(closeSearchBtn);
     }
 })
 
-closeBtn.addEventListener("click", (e)=> {
+/* Event that causes the closeSearchBtn button delete the text that is in the search input */
+closeSearchBtn.addEventListener("click", (e)=> {
     e.preventDefault();
 
     inputSearch.value = "";
-    closeBtn.style.display = "none";
+    hiddenState(closeSearchBtn);
     inputSearch.focus();
 })
